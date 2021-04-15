6abeecf2a32d8e1e3049386cc47d5efb44825d8a
yesanhueza <yesanhueza@falabella.cl>
2021-04-14 20:49:08 -0400

    refactor: change version typeorm

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -2,17 +2,22 @@ import { createConnection, Connection } from 'typeorm';
 
 import * as entities from 'entities';
 
-const createDatabaseConnection = (): Promise<Connection> =>
-  createConnection({
+const createDatabaseConnection = (): Promise<Connection> => {
+  console.log(process.env.DB_HOST.replace(/\n/g, ''));
+  console.log(process.env.DB_PORT.replace(/\n/g, ''));
+  console.log(process.env.DB_USERNAME.replace(/\n/g, ''));
+  console.log(process.env.DB_PASSWORD.replace(/\n/g, ''));
+  console.log(process.env.DB_DATABASE.replace(/\n/g, ''));
+  return createConnection({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST.replace(/\n/g, ''),
+    port: Number(process.env.DB_PORT.replace(/\n/g, '')),
+    username: process.env.DB_USERNAME.replace(/\n/g, ''),
+    password: process.env.DB_PASSWORD.replace(/\n/g, ''),
+    database: process.env.DB_DATABASE.replace(/\n/g, ''),
     entities: Object.values(entities),
     synchronize: true,
-    ssl: true,
   });
+};
 
 export default createDatabaseConnection;
